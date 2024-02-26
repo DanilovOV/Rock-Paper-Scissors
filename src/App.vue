14447148e1b4e64f1b5f54ee3546269b9056bdd2
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-26 17:21:58 +0300

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from './components/TheHeader.vue'
+import { ref } from 'vue'
+
+const score = ref<number>(0)
+</script>
 
 <template>
-	<div>test</div>
+	<div class="wrapper">
+		<div class="container">
+			<TheHeader :score="score" :mode="'bonus'" />
+		</div>
+	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrapper {
+	margin: 60px 0;
+}
+.container {
+	max-width: min(80vw, 800px);
+	margin: 0 auto;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import bonusLogo from '@/assets/icons/logo-bonus.svg'
+import logo from '@/assets/icons/logo.svg'
+
+export interface Props {
+	score: number
+	mode?: 'standart' | 'bonus'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	mode: 'standart',
+})
+
+const emits = defineEmits<{
+	changeMode: [bonus: 'standart' | 'bonus']
+}>()
+</script>
+
+<template>
+	<section class="header">
+		<h1 class="header__title">
+			<img
+				:src="mode === 'standart' ? logo : bonusLogo"
+				alt="Rock, Paper, Scissors logo"
+				class="header__title-img"
+			/>
+		</h1>
+
+		<div class="header__score">
+			<h2 class="header__score-label">Счет</h2>
+			<span class="header__score-value">{{ props.score }}</span>
+		</div>
+	</section>
+</template>
+
+<style scoped lang="scss">
+.header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	gap: 20px;
+	border: 2px solid #46556f;
+	border-radius: 10px;
+	padding: 0 30px;
+
+	&__title {
+		display: flex;
+		align-items: center;
+	}
+
+	&__title-img {
+		max-height: 100px;
+	}
+
+	&__score {
+		display: flex;
+		flex-flow: column nowrap;
+
+		background-color: white;
+		text-align: center;
+		padding: 20px 35px;
+		margin: 20px 0;
+		border-radius: 0.25em;
+		line-height: 0.9;
+		gap: 0.2em;
+	}
+
+	&__score-label {
+		color: #0027c5;
+		font-weight: bold;
+		font-size: 15px;
+		text-transform: uppercase;
+		letter-spacing: 0.3px;
+	}
+
+	&__score-value {
+		margin-top: 4px;
+		font-size: 30px;
+		color: #3a4262;
+	}
+}
+</style>
