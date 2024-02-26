f69b3765196ba5660157e3dd79a0c97ac8b3ca58
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-26 22:04:52 +0300

    update GameButton styles

--- src/components/GameButton.vue
+++ src/components/GameButton.vue
@@ -7,18 +7,42 @@ defineProps<{ data: GameElementValue }>()
 
 <template>
 	<div class="game-button" :style="{ background: data.color }">
-		<AppIcon :name="data.icon" />
+		<div class="game-button__inner">
+			<AppIcon :name="data.icon" />
+		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
 .game-button {
+	$this: &;
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	width: 100px;
-	height: 100px;
+	width: 125px;
+	height: 125px;
 	border-radius: 50%;
-	background: #fff;
+	box-shadow: inset 0 -4px 2px 1px rgba(0, 0, 0, 0.3);
+	cursor: pointer;
+
+	&:hover {
+		#{$this}__inner {
+			opacity: 0.85;
+			box-shadow: inset 0 4px 4px 4px rgba(0, 0, 0, 0.2);
+		}
+	}
+
+	&__inner {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100px;
+		height: 100px;
+		border-radius: 50%;
+		background: var(--c-white);
+		color: var(--c-dark);
+		box-shadow: inset 0 4px 2px 1px rgba(0, 0, 0, 0.2);
+		transition: all 0.3s ease;
+	}
 }
 </style>
