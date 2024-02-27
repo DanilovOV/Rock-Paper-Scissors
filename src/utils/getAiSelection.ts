91cc34b4bece44135ffc1cf7d87de20fbe011bb5
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-27 18:08:40 +0300

    fixed ai selection

--- src/utils/getAiSelection.ts
+++ src/utils/getAiSelection.ts
@@ -2,7 +2,8 @@ import { gameElements } from '@/gameElements'
 
 export const getAiSelection = (mode: GameMode) => {
 	const selectionNumber = Math.floor(
-		Math.random() * (mode === 'standart' ? 2 : 4)
+		Math.random() * (mode === 'standart' ? 3 : 5)
 	)
+
 	return Object.keys(gameElements)[selectionNumber] as GameEntities
 }
